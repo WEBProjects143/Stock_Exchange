3ffbda9aacd3d478a5c4ea9f97127bf6682272bd
Bhupendra yadav <146579008+WEBProjects143@users.noreply.github.com>
2025-03-08 12:38:48 +0530

    Update Pg.js

--- Project/Db/Pg.js
+++ Project/Db/Pg.js
@@ -1,12 +1,15 @@
 
 const { Client } = require('pg');
     const client = new Client({
-        user: 'postgres',       
-        host: 'localhost',     
-        database: 'Bhupendra', 
-        password: '1234',
-        port: 5432,
-      });
+       user: process.env.user,       
+        host: process.env.host,     
+        database: process.env.database, 
+        password: process.env.password,
+        port: process.env.port,
+        ssl: {
+          rejectUnauthorized: false,  // Allows the connection even if the certificate is self-signed
+        }
+    });
       const connectDb = async () => {
         try {
           await client.connect();
